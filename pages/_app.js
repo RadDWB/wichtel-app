fcf19e0d81d664b0ec81c01b918944c79984d3e0
RadDWB <github@oneonone.de>
2025-11-06 09:36:45 +0100

    Update import comment for Home.module.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
 import '../styles/globals.css';
-import '../styles/Home.module.css'; // Optional, falls du es brauchst
+import '../styles/Home.module.css'; // Optional: Falls du Module-CSS brauchst
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
